e27ca8390927ee50bfa20346b0a779cf65b50bc1
bro abbas <75854041+abbasalkabbi@users.noreply.github.com>
2021-09-05 19:42:37 +0300

    v-5
    style for moblie (skills-icon)and navbar

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
-const menu = document.querySelector(".menu-nav"),
+var menu = document.querySelector(".menu-nav"),
   a = menu.querySelectorAll("ul a");
 a[0].addEventListener("click", show);
 a[1].addEventListener("click", show);
 a[2].addEventListener("click", show);
 a[3].addEventListener("click", show);
+var home=document.querySelector(".home");
+var skills=document.querySelector(".skills");
+var about=document.querySelector(".about");
+var contact=document.querySelector(".contact");
 function show() {
-  console.log(a);
-  if (menu.style.display == "none") {
-    menu.style.display = "flex";
-  } else {
-    menu.style.display = "none";
-  }
+
+  menu.style.display = "none";
+  
 }
 
 function menubutton() {
-  show();
+  menu.style.display = "flex";
 }
+function menubuttonclose(){
+  menu.style.display = "none";
+}
+
+
+
+function isInViewport(elem) {
+  let bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= -100 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right-100 <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+window.addEventListener('scroll', function (event) {
+  let home_nav=document.querySelector(".home-nav"),
+  skills_nav=document.querySelector(".Skills-nav"),
+  about_nav=document.querySelector(".about-nav"),
+  contact_nav=document.querySelector(".contact-nav");
+  
+	if (isInViewport(home)) {
+    home_nav.classList.add("active")
+		skills_nav.classList.remove("active")
+    about_nav.classList.remove("active")
+    contact_nav.classList.remove("active")
+
+	}
+  if (isInViewport(skills)) {
+    home_nav.classList.remove("active")
+		skills_nav.classList.add("active")
+    about_nav.classList.remove("active")
+    contact_nav.classList.remove("active")
+	}
+  if (isInViewport(about)) {
+    home_nav.classList.remove("active")
+		skills_nav.classList.remove("active")
+    about_nav.classList.add("active")
+    contact_nav.classList.remove("active")
+	}
+  if (isInViewport(contact)) {
+    home_nav.classList.remove("active")
+		skills_nav.classList.remove("active")
+    about_nav.classList.remove("active")
+    contact_nav.classList.add("active")
+	}
+}, false);
